5c736802454d1866d354e356ce1e6e73f662ba65
RafaelRResnauer <resnauer.rafael@gmail.com>
2021-01-29 12:41:22 -0300

    desenvolvimento inicial de personagem

---
+++ script.js
@@ -0,0 +1,10 @@
+const player_char = document.querySelector('.player_char');
+
+function handleKeyup(event) {
+    if(event.keyCode === ) {
+
+    }
+
+} 
+
+document.addEventListener('keyup', handleKeyup);
