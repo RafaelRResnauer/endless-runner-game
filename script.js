789b87a45f395a9437ece78dc2e04e06147c799e
RafaelRResnauer <resnauer.rafael@gmail.com>
2021-01-29 21:23:24 -0300

    implementação do básico do jogo

--- script.js
+++ script.js
@@ -1,37 +1,80 @@
+// var que representa o personagem do jogador
 const player_char = document.querySelector('.player_char');
+const static_background = document.querySelector('.static_background');
+let isJumping = false;
+// posicao do personagem
+let charPosition = 0;
 
-/* Function to handle key presses */
+/* Funcao para tratar pressionamento de tecla */
 function handleKeyup(event) {
     if(event.keyCode === 32) {
-        jump();
+        if(!isJumping){
+
+            jump();
+        }
     }
 } 
-/* Function to handle jumping */
+
+/* Funcao para lidar com pulos */
 function jump() {
-    let position = 0;
-    // going up
+    // personagem esta pulando
+    isJumping = true;
+
+    // indo para cima
     let upInterval = setInterval(() => {
-        // checks if max height is achieved
-        if(position >= 150){
-            // clears interval
+        // verifica se chegou na altura maxima
+        if(charPosition >= 150){
+            // limpa intervalo
             clearInterval(upInterval);
-            // going down
+            // indo para baixo
             let downInterval = setInterval(() => {
-                // checks if min height is achieved
-                if(position <= 0){
-                    // clears interval
+                // verifica se posicao minima foi atingida
+                if(charPosition <= 0){
+                    // limpa intervalo
                     clearInterval(downInterval);
+
+                    // personagem chegou ao chao
+                    isJumping = false;
                 }else{
-                    // goes down
-                    position -= 20;
-                    player_char.style.bottom = position + 'px';
+                    // vai para baixo
+                    charPosition -= 20;
+                    player_char.style.bottom = charPosition + 'px';
                 }
             }, 20);
         }else {
-            // goes up
-            position += 20;
-            player_char.style.bottom = position + 'px';
+            // vai para cima
+            charPosition += 20;
+            player_char.style.bottom = charPosition + 'px';
         }
     }, 20);
 }
+
+function createCactus() {
+    const cactus = document.createElement('div');
+    let cactusPosition= 1000;
+    let randomTime = Math.random() * 6000;
+
+    cactus.classList.add('cactus_obstacle');
+    cactus.style.left = 1000 + 'px';
+    static_background.appendChild(cactus);
+
+    let leftInterval = setInterval(() => {
+        if(cactusPosition < -60) {
+            clearInterval(leftInterval);
+            static_background.removeChild(cactus);
+        }else if(cactusPosition > 0 && cactusPosition < 60 && charPosition < 60){
+            clearInterval(leftInterval);
+            document.body.innerHTML = '<h1 class="game_over">Fim de Jogo</h1>';
+        }else {
+            cactusPosition -= 20;
+            cactus.style.left = cactusPosition + 'px';
+        }
+    }, 20);
+
+    setTimeout(createCactus, randomTime);
+}
+
+createCactus();
+
+// verifica se tecla foi pressionada
 document.addEventListener('keyup', handleKeyup);
